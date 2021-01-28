86cdeab0c81ab93fa5bd095a01904f22e9303d7a
beans3 <beans3.k@gmail.com>
2021-01-28 22:58:30 +0900

    20210128_콜백다시

--- js10.js
+++ js10.js
@@ -1,37 +1,36 @@
 // 1. Object to JSON
 // stringfy(obj)
-let json = JSON.stringify(true);
-console.log(json);
+let json = JSON.stringify(true)
+console.log(json)
 
-json = JSON.stringify(['apple', 'banana']);
-console.log(json);
+json = JSON.stringify(['apple', 'banana'])
+console.log(json)
 
 const rabbit = {
-    name: 'tori',
-    color: 'whtie',
-    size: null,
-    birthDate: new Date(),
-    //symbole: Symbol('id'),
-    jump: () => {
-        console.log(`${name} can jump!`);
-    },
-};
+  name: 'tori',
+  color: 'whtie',
+  size: null,
+  birthDate: new Date(),
+  //symbole: Symbol('id'),
+  jump: () => {
+    console.log(`${name} can jump!`)
+  }
+}
 
-json = JSON.stringify(rabbit);
-console.log(json);
+json = JSON.stringify(rabbit)
+console.log(json)
 
-json = JSON.stringify(rabbit, ['name']);
-console.log(json);
+json = JSON.stringify(rabbit, ['name'])
+console.log(json)
 
-json = JSON.stringify(rabbit, ['name', 'color', 'size']);
-console.log(json);
+json = JSON.stringify(rabbit, ['name', 'color', 'size'])
+console.log(json)
 
 json = JSON.stringify(rabbit, (key, value) => {
-    console.log(`key: ${key}, value: ${value}`);
-    return key === 'name' ? 'ellie' : value;
-});
+  console.log(`key: ${key}, value: ${value}`)
+  return key === 'name' ? 'ellie' : value
+})
 
-console.log(json);
-
+console.log(json)
 
 // 2. JSON to Object
